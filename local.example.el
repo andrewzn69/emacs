dd6f71b1e317198dcb09127b300dd1e7bd3c26cf
Ondřej Zeman <ondrejzeman@zemn.xyz>
2026-09-14 00:23:56 +0200

    add local settings template

---
+++ local.example.el
@@ -0,0 +1,14 @@
+;;; local.el --- Settings for this machine only -*- lexical-binding: t; -*-
+
+;; copy to local.el and uncomment what this machine needs
+
+;; skip a module
+;; (setq my/modules (remove 'org my/modules))
+
+;; theme and font
+;; (setq my/theme 'gruvbox-dark-soft)
+;; (setq my/font-family "JetBrainsMono Nerd Font")
+;; (setq my/font-height 160)
+
+;; notes dir
+;; (setq my/org-directory "~/notes/")
