962a62d0031c30cefde1d7f48fb3d17f6150105f
Ondřej Zeman <ondrejzeman@zemn.xyz>
2026-09-14 00:22:56 +0200

    add evil keys

---
+++ modules/evil.el
@@ -0,0 +1,19 @@
+;;; evil.el --- Vim keys -*- lexical-binding: t; -*-
+
+(use-package evil
+	:init
+	;; keys for other modes come from evil collection instead
+	(setq evil-want-keybinding nil)
+	:custom
+	;; C-u scrolls up, Y yanks to end of line
+	(evil-want-C-u-scroll t)
+	(evil-want-Y-yank-to-eol t)
+	;; C-r redo through the built in undo redo
+	(evil-undo-system 'undo-redo)
+	:config
+	(evil-mode 1))
+
+(use-package evil-collection
+	:after evil
+	:config
+	(evil-collection-init))
