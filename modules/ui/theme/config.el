d00ded60569e5c88f6fb598825c5e1b9eeec874b
Ondřej Zeman <ondrejzeman@zemn.xyz>
2026-09-23 12:59:02 +0200

    add everforest theme option

--- modules/ui/theme/config.el
+++ modules/ui/theme/config.el
@@ -5,4 +5,13 @@
 
 ;; themes shipped in this repo
 (add-to-list 'custom-theme-load-path (expand-file-name "themes" my/config-directory))
+
+;; cloned only where it is picked, the recipe carries the source because it is not on melpa
+(when (string-prefix-p "everforest" (symbol-name my/theme))
+	(straight-use-package '(everforest :type git :host github :repo "theorytoe/everforest-emacs"))
+	;; straight builds outside the theme search path
+	(let ((file (locate-library "everforest-hard-dark-theme")))
+		(when file
+			(add-to-list 'custom-theme-load-path (file-name-directory file)))))
+
 (load-theme my/theme t)
