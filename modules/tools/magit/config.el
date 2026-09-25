8f58934139bce8045332d11d8017fcd8a6187efc
Ondřej Zeman <ondrejzeman@zemn.xyz>
2026-09-25 23:40:19 +0200

    move magit into tools category

--- modules/tools/magit/config.el
+++ modules/tools/magit/config.el
@@ -1,4 +1,4 @@
-;;; git.el --- Git client -*- lexical-binding: t; -*-
+;;; config.el --- Git client -*- lexical-binding: t; -*-
 
 (use-package magit
   ;; loads on the first magit command, C-x g or leader g g opens the status buffer
